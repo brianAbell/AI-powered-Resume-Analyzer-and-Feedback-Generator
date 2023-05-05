f4151bf6ecfcb60410a8f13d340f20c6fbaa7c92
Brian <brianbell212@gmail.com>
2023-05-05 11:31:47 -0400

    updated upload resume function with gpt4 api call

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -1,4 +1,3 @@
-
 //DOMContentLoaded event to ensure that the HTML document has been fully loaded
 //and parsed before attempting to access any elements
 //Task: this code first gets the reference to the button element then it attaches
@@ -10,19 +9,24 @@ document.addEventListener("DOMContentLoaded", function () {
     uploadButton.addEventListener("click", uploadResume);
 });
 
+function uploadResume() {
+    console.log("Botton clicked!");
 
-//event listener for button / button functionality
-uploadButton.addEventListener('click', () => {
-    fileInput.click();
-});
+    const inputText = "Your prompt text goes here";
 
-//handle file selection
-fileInput.addEventListener('change', (event) => {
-    const file = event.target.files[0];
-    if(file) {
-        //TODO: we process the resume here / add gpt4 API usage
-        console.log('Selected file: ${file.name}');
-    } else {
-        console.log('No file selected');
-    }
-});
+    // Call the backend route and process the response
+    fetch("/gpt4", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ input: inputText }),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("GPT-4 response:", data.output);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+}
