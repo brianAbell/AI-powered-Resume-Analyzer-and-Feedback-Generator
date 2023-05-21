9222c31ccd07ef1ef7dee1e869f52abeeb1c44e5
Brian <brianbell212@gmail.com>
2023-05-21 14:27:45 -0400

    Add text extraction from PDF files in API

--- back-end/app.py
+++ back-end/app.py
@@ -6,6 +6,8 @@
 from flask import Flask, request, jsonify
 import openai
 import os
+import PyPDF2
+from io import BytesIO
 
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
@@ -15,6 +17,36 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/process-file', methods=['POST'])
+def process_file():
+    # Get the base64-encoded file content from the request's JSON payload
+    file_data = request.json.get('fileContent', '')
+
+    # Decode the base64 string to get the binary data
+    file_bytes = BytesIO(base64.b64decode(file_data))
+    
+    # If we received some data
+    if file_bytes:
+        # Create a PDF file reader object
+        pdf_file = PyPDF2.PdfFileReader(file_bytes)
+
+        # Initialize an empty string to store the extracted text
+        text = ''
+
+        # Loop over all the pages in the PDF file
+        for page in range(pdf_file.getNumPages()):
+            # Extract the text from the current page and append it to our text string
+            text += pdf_file.getPage(page).extractText()
+        
+        # Return the extracted text in a JSON response
+        return jsonify({'extractedText': text})
+    
+    # If we didn't receive any data, return an error message
+    else:
+        return jsonify({'error': 'Invalid file content.'}), 400
+
+
 
 #new route for GPT API requests
 #NOTE: This code defines a new route named /gpt4 that accepts POST requests, 
@@ -38,6 +70,20 @@ def generate_text():
     reply = response['choices'][0]['message']['content']
 
     # Here we are returning the reply as a JSON response
+    return jsonify({'reply': reply})
+
+# send the extracted text as a prompt to the API
+@app.route('/get-gpt-response', methods=['POST'])
+def get_gpt_response():
+    input_text = request.json['input_text']
+    response = openai.ChatCompletion.create(
+        model="gpt4",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": input_text},
+        ]
+    )
+    reply = response['choices'][0]['message']['content']
     return jsonify({'reply': reply})
 
 if __name__ == '__main__':
