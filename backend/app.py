b60f974d02c7013c60dfc14c9071b735993f8de4
Brian Bell <bbell.semm@gmail.com>
2023-10-25 15:04:17 -0400

    Code & comment restructure

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,6 @@
 import base64
 import os
 from flask_cors import CORS
-
 from io import BytesIO
 import PyPDF2
 import openai
@@ -14,30 +13,37 @@ load_dotenv()
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-
+# Set up OpenAI key
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-
 app = Flask(__name__)
-CORS(app) # used to resolve front/back-end communication issues
+CORS(app)  # Used to resolve front/back-end communication issues
 
 # INTRO TESTING ______________________________________________
 @app.route('/')
 def hello_world():
+    """
+    Basic endpoint to check if the API is up and running.
+    :return: A hello world string.
+    """
     return 'Hello, World!'
-# ____________________________________________________________
+
 
 # FILE PROCESSING ____________________________________________
-# NOTE: endpoint accepts a POST req w base64 encoded PDF.
-# NOTE: then decodes file, extract text using PyPDF2, returns text
 @app.route('/process-file', methods=['POST'])
 def process_file():
+    """
+    Process an uploaded PDF by extracting its text.
+    :param: A JSON payload containing a base64 encoded PDF.
+    :return: A JSON response with the extracted text or an error message.
+    """
     logging.info("Received a request to process a PDF.")
+    
     try:
         # Get the base64-encoded file content from the request's JSON payload
         file_data = request.json.get('fileContent', '')
 
-        # Check if the data starts with the known prefix and if so, strip it
+        # Check and strip base64 prefix
         if file_data.startswith("data:application/pdf;base64,"):
             file_data = file_data.split(",")[1]
 
@@ -48,48 +54,41 @@ def process_file():
 
         logging.info(f"Decoded file bytes: {file_bytes.getvalue()[:100]}...")  # Print the first 100 bytes
         
-        # If we received some data
+        # Extract text from PDF if data is received
         if file_bytes:
-            # Create a PDF file reader object
             pdf_file = PyPDF2.PdfReader(file_bytes)
-
-            # Initialize an empty string to store the extracted text
             text = ''
-
-            # Loop over all the pages in the PDF file
             for page in range(len(pdf_file.pages)):
-                # Extract the text from the current page and append it to our text string
                 text += pdf_file.pages[page].extract_text()
             
             logging.info(f"Successfully processed a PDF with {len(pdf_file.pages)} pages.")
-            logging.info(f"Extracted text: {text[:100]}...")  # Print the first 100 characters
-            # Return the extracted text in a JSON response
             return jsonify({'extractedText': text})
-        
-        # If we didn't receive any data, return an error message
         else:
             return jsonify({'error': 'Invalid file content.'}), 400
+
     except Exception as e:
         logging.error(f"Error processing file: {e}")
 
         # Check if the error is a PdfReadError
         if 'EOF marker not found' in str(e):
             return jsonify({'error': 'Invalid or corrupt PDF file.'}), 400
-        # You may add more conditions here to handle different errors differently
-        # Else, return a general server error
         else:
             return jsonify({'error': 'An error occurred processing the file.'}), 500
-# ____________________________________________________________
+
 
 # GPT4 INTERACTION ____________________________________________
 @app.route('/resume-feedback', methods=['POST'])
 def get_resume_feedback():
-    # Extract the resume text
+    """
+    Sends the parsed resume to GPT-4 for analysis and returns feedback.
+    :param: A JSON payload containing the input text (resume content).
+    :return: A JSON response with GPT-4 feedback or an error message.
+    """
     input_text = request.json.get('input_text', '')
     if not input_text:
         return jsonify({'error': 'Input text is required.'}), 400
 
-    # Send parsed resume AND instructions to GPT-4 for analysis
+    # Send parsed resume to GPT-4 for analysis
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
@@ -102,7 +101,7 @@ def get_resume_feedback():
     reply = response['choices'][0]['message']['content']
     logging.info("Successfully obtained feedback from GPT-4 for a resume.")
     return jsonify({'reply': reply})
-# _____________________________________________________________
+
 
 if __name__ == '__main__':
     app.run(debug=True)
